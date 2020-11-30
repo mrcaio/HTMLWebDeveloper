9a7ee96d119a8148c053d7b46456525e22c72804
Caio <caiobaader@gmail.com>
2020-11-30 10:37:56 -0300

    desenvolvapáginascomjs

--- programcaoparainternetjs/js/main.js
+++ programcaoparainternetjs/js/main.js
@@ -1,5 +1,38 @@
 
-var d = new Date();
+function soma(n1, n2) {
+    return n1+n2;
+}
+
+function validaIdade (idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    }
+    else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?")
+console.log(validaidade(idade));
+
+//alert (soma(5,10));
+
+
+/*function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+alert (soma(5,10));
+alert (setReplace ("Vai Japão", "Japão", "Brasil");
+
+
+
+
+
+
+/*var d = new Date();
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
