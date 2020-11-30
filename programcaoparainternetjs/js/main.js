55bad70697933a30425e22ffcdbf5955880ba0bf
Caio <caiobaader@gmail.com>
2020-11-30 08:29:18 -0300

    introdução ao js

---
+++ programcaoparainternetjs/js/main.js
@@ -0,0 +1,10 @@
+var nome = "Rafael Galleani";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + "tem" + idade + "anos");
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade*idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"))
