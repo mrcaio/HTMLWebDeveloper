9d32985b5da0e0e4dac9ba7d24a9057daa87ce34
Caio <caiobaader@gmail.com>
2020-11-30 08:39:02 -0300

    array e dicionário

--- programcaoparainternetjs/js/main.js
+++ programcaoparainternetjs/js/main.js
@@ -1,4 +1,24 @@
-var nome = "Rafael Galleani";
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista);
+//console.log(lista.join("-"));
+
+
+
+
+/*var nome = "Rafael Galleani";
 var idade = 29;
 var idade2 = 10;
 var frase = "Japão é o melhor time do mundo"
@@ -7,4 +27,4 @@ var frase = "Japão é o melhor time do mundo"
 console.log(nome);
 console.log(idade*idade2);
 console.log(frase.replace("Japão", "Brasil"));
-//alert(frase.replace("Japão", "Brasil"))
+alert(frase.replace("Japão", "Brasil"))*/
